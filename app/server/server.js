f0ccb30117318705fe99c1839907bbea35e1faf8
Yusuke Kawamoto <novogrammer@gmail.com>
2021-09-13 01:45:52 +0900

    server.jsの書き方を変更

--- app/server/server.js
+++ app/server/server.js
@@ -1,21 +1,30 @@
-//see https://github.com/vercel/next.js/blob/canary/examples/custom-server-express/server.js
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
+const http = require('http');
 
-const port = 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
 
-app.prepare().then(() => {
-  const server = express()
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
 
+async function main(){
+  const expressApp = express();
+  const server=http.createServer(expressApp);
+  {
+    const nextApp = next({ dev });
+    const nextHandler = nextApp.getRequestHandler();
+    await nextApp.prepare();
+    
+    expressApp.all('*', (req, res) => {
+      return nextHandler(req, res);
+    });
+  }
+  
   server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+  
+}
+
+main();
