b8cd973f8371678375fc2e5892b6514d653f0dfa
Yusuke Kawamoto <novogrammer@gmail.com>
2021-09-20 05:33:59 +0900

    解像度を下げると複数peerで処理落ちしないことがわかった。

--- app/common/constants.js
+++ app/common/constants.js
@@ -1,5 +1,5 @@
 export const FPS_SERVER = 10;
-export const VIDEO_SIZE = 512;
+export const VIDEO_SIZE = 128;
 
 export const EVENT_NEED_TO_CONNECT ="need to connect";
 export const EVENT_NEED_TO_DISCONNECT ="need to disconnect";
